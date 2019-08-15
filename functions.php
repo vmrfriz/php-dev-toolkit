70fe5f8587f818d9c017b16b05b51504a53fb9bd
Valeriy Grechukha <v.mrfriz@gmail.com>
2019-08-15 12:40:19 +0300

    Initial commit

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function head() {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="http://promtrans.pro/cargo/classes/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
 
     <title>DEV</title>
 
